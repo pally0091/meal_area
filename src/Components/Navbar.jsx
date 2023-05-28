02337ad829ecb3dd40ed3e170bfee3346bacffe6
Debashis Roy <goodquote39@hotmail.com>
2023-05-29 02:59:26 +0600

    little changes on animations

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className='flex lg:flex-row md:flex-row sm:flex-col min-[320px]:flex-col justify-between px-8 py-4 bg-gradient-to-r from-rose-500  to-violet-300 items-center shadow-xl shadow-yellow-300 mb-5 '>
+        <div className='flex lg:flex-row md:flex-row sm:flex-col min-[320px]:flex-col justify-between px-8 py-4 bg-gradient-to-r from-lime-500 via-rose-200 to-violet-500 items-center shadow-xl shadow-yellow-300 mb-5 '>
             <div>
                 <h1 className='text-5xl italic font-semibold'>Meals In <span className='text-violet-500 font-bold'>Area</span></h1>
                 <p className='text-center text-lg' id='slogan'>Find meals where you at</p>
