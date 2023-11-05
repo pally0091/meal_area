6ad605275259ebd9f8650c6140e68b625b3ada32
Debashis Roy <goodquote39@hotmail.com>
2023-11-05 23:39:57 +0600

    some color change and button effects change

--- src/Components/Areas.jsx
+++ src/Components/Areas.jsx
@@ -1,32 +1,27 @@
-
-import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Areas = () => {
-    const [areas, setAreas] = useState();
-    useEffect(() => {
-        fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
-            .then(res => res.json())
-            .then(data => setAreas(data?.meals))
-    }, []);
-    // console.log(areas);
-    return (
-        <div className='flex flex-col p-4 overflow-scroll h-[80%] overflow-x-hidden sticky top-0'>
-            {
-                areas?.map(area => (
-                    <NavLink
-                        
-                        key={area?.strArea}
-                        to={`/${area?.strArea}`}
-                        className='text-lg bg-gradient-to-r from-blue-400 to-green-300 my-1 p-2 rounded-e-full text-center area-name  hover:from-green-300 hover:to-blue-400 transition duration-700'
-                    >
-                        
-                    { area?.strArea}
-                    </NavLink>
-               ))
-            }
-        </div>
-    );
+  const [areas, setAreas] = useState();
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+      .then((res) => res.json())
+      .then((data) => setAreas(data?.meals));
+  }, []);
+  // console.log(areas);
+  return (
+    <div className="flex flex-col p-4 overflow-scroll h-[80%] overflow-x-hidden sticky top-0">
+      {areas?.map((area) => (
+        <NavLink
+          key={area?.strArea}
+          to={`/${area?.strArea}`}
+          className="text-lg bg-gradient-to-r from-blue-400 to-amber-300 my-1 p-2 rounded-e-full text-center area-name  hover:from-amber-300 hover:to-blue-400 transition duration-700"
+        >
+          {area?.strArea}
+        </NavLink>
+      ))}
+    </div>
+  );
 };
 
 export default Areas;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div className='flex lg:flex-row md:flex-row sm:flex-col min-[320px]:flex-col justify-between px-8 py-4 bg-gradient-to-r from-lime-500 via-rose-200 to-violet-500 items-center shadow-xl shadow-yellow-300 mb-5 '>
-            <div>
-                <h1 className='text-5xl italic font-semibold'>Meals In <span className='text-violet-500 font-bold'>Area</span></h1>
-                <p className='text-center text-lg' id='slogan'>Find meals where you at</p>
-            </div> 
-            <div className='mt-5'>
-                <NavLink className='text-gray-900 bg-gradient-to-r from-rose-500 via-white to-violet-300 hover:bg-gradient-to-br  shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg px-5 py-2.5 text-center m-3 text-xl  ' to='/'>Home</NavLink>
-                <NavLink className='text-gray-900 bg-gradient-to-r from-rose-500 via-white to-violet-300 hover:bg-gradient-to-br  shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg px-5 py-2.5 text-center m-3 text-xl ' to='/blogs'>Facts</NavLink>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex lg:flex-row md:flex-row sm:flex-col min-[320px]:flex-col justify-between px-8 py-4 bg-gradient-to-r from-lime-500 via-rose-200 to-violet-500 items-center shadow-xl shadow-yellow-300 mb-5 ">
+      <div>
+        <h1 className="text-5xl italic font-semibold">
+          Meals In <span className="text-violet-500 font-bold">Area</span>
+        </h1>
+        <p
+          className="text-center text-lg"
+          id="slogan"
+        >
+          Find meals where you at
+        </p>
+      </div>
+      <div className="mt-5">
+        <NavLink
+          className=" head-link text-gray-900 bg-gradient-to-r from-rose-500 via-white to-violet-300 hover:bg-gradient-to-br  shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg px-5 py-2.5 text-center m-3 text-xl  "
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className=" head-link text-gray-900 bg-gradient-to-r from-rose-500 via-white to-violet-300 hover:bg-gradient-to-br  shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg px-5 py-2.5 text-center m-3 text-xl "
+          to="/blogs"
+        >
+          Facts
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
